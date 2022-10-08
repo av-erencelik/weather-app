add385129b3ffc9bdf7d730af7fae495c0dc3400
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-10-08 12:50:29 +0300

    added error handling for async function

--- src/script.js
+++ src/script.js
@@ -9,14 +9,22 @@ import thunderstorm from "./img/thunderstorm.jpeg";
 import snow from "./img/snow.jpeg";
 import mist from "./img/mist.jpg";
 import weather from "./img/weather.jpg";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 
 const cityInput = document.getElementById("city");
 const form = document.getElementById("form");
+const error = document.getElementById("error");
 
 getWeatherInfos("istanbul");
 
 form.onsubmit = (e) => {
   e.preventDefault();
+  if (error.textContent != "") {
+    error.textContent = "";
+  }
   let city = cityInput.value;
   cityInput.value = "";
   city = capitalizeFirstLetter(city);
@@ -25,13 +33,17 @@ form.onsubmit = (e) => {
 };
 
 async function getWeatherInfos(cityName) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=71d372d0720a373c87565c0bc92d08ab&units=metric`,
-    { mode: "cors" }
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
-  handleData(weatherData);
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=71d372d0720a373c87565c0bc92d08ab&units=metric`,
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    handleData(weatherData);
+  } catch (err) {
+    error.textContent = "Please Enter Valid Location!";
+  }
 }
 function capitalizeFirstLetter(string) {
   string = string[0].toUpperCase() + string.slice(1);
